23bc1b1d640e911701f5dd5344750d3670a27f45
amkhrjee <aniruddha.mukherji@outlook.com>
2025-08-25 04:58:01 +0530

    add: splc

---
+++ solutions/splc.py
@@ -0,0 +1,88 @@
+from Bio import SeqIO
+
+codon_table = {
+    "UUU": "F",
+    "UUC": "F",
+    "UUA": "L",
+    "UUG": "L",
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+    "AUG": "M",
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "UAU": "Y",
+    "UAC": "Y",
+    "UAA": "Stop",
+    "UAG": "Stop",
+    "CAU": "H",
+    "CAC": "H",
+    "CAA": "Q",
+    "CAG": "Q",
+    "AAU": "N",
+    "AAC": "N",
+    "AAA": "K",
+    "AAG": "K",
+    "GAU": "D",
+    "GAC": "D",
+    "GAA": "E",
+    "GAG": "E",
+    "UGU": "C",
+    "UGC": "C",
+    "UGA": "Stop",
+    "UGG": "W",
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",
+    "AGU": "S",
+    "AGC": "S",
+    "AGA": "R",
+    "AGG": "R",
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+}
+
+seqs = []
+
+for record in SeqIO.parse("./datasets/example.fasta", "fasta"):
+    seqs.append(record.seq)
+
+s, introns = seqs[0], seqs[1:]
+
+for intron in introns:
+    s = s.replace(intron, "")
+
+mrna = s.replace("T", "U")
+
+
+protein = ""
+for i in range(0, len(mrna) - 3, 3):
+    aa = codon_table[mrna[i : i + 3]]
+    protein += aa
+
+print(protein)
